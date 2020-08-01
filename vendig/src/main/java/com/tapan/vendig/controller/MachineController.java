8063326a6fc161ab5c1c11278fcaf0645cb5e1a8
Tapan Kumar Achary <TapanA1@verifone.com>
2020-08-02 02:30:02 +0530

    vending

---
+++ vendig/src/main/java/com/tapan/vendig/controller/MachineController.java
@@ -0,0 +1,36 @@
+package com.tapan.vendig.controller;
+
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.tapan.vendig.model.Order;
+import com.tapan.vendig.model.ReturnItems;
+import com.tapan.vendig.sevice.VendingService;
+
+@RestController
+@RequestMapping("/api")
+public class MachineController {
+	@Autowired
+	private VendingService VendingService;
+	
+	
+	@PostMapping("/order")
+	public ReturnItems processRequest(@RequestBody Order order) {
+		
+		if(VendingService.checkItemExistInMachine(order.getItem())) {
+			Map<String, Integer> map= VendingService.getBalanceAmount(order);	
+			return new ReturnItems(map,order.getItem());
+		}
+		else {
+			Map<String, Integer> map= VendingService.coinsReceivedFromOrder(order);
+			return new ReturnItems(map,"The item: "+order.getItem()+ " does not exit in Vending machine, so returning all change provided");
+		}
+		
+		
+	}
+
+}
